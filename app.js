1858556cab18f81809e9b772538968e93f902ec9
Shun <ter0123540@gmail.com>
2022-04-21 20:52:44 +0800

    add setCustomizedCanvas function

--- app.js
+++ app.js
@@ -1,21 +1,48 @@
 const container = document.querySelector('.container');
 
-for (let i = 0; i < 256; i++) {
-    putDiv();
+let customizedSize = prompt('Please enter the size of number');
+
+setInitialCanvas();
+
+
+
+setCustomizedCanvas();
+printDot();
+
+
+
+
+
+
+
+function setInitialCanvas() {
+    for (let i = 0; i < 16*16; i++) {
+        putDiv();
+    }
+    container.classList.add('gridBoard');
 }
 
-const  divs = document.querySelectorAll('div > div');
-divs.forEach(div => {
-    div.addEventListener('mouseover', (e) => {
-        e.target.style.backgroundColor = '#fa0';
+function setCustomizedCanvas() {
+    for (let i = 0; i < (customizedSize*customizedSize); i++) {
+        putDiv();
+    }
+    container.style.gridTemplateColumns = `repeat(${customizedSize}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${customizedSize}, 1fr)`;
+}
+
+function printDot() {
+    const  divs = document.querySelectorAll('div > div');
+    divs.forEach(div => {
+        div.addEventListener('mouseover', (e) => {
+            e.target.style.backgroundColor = '#fa0';
+        })
     })
-})
+}
 
 function putDiv() {
     const div = document.createElement('div');
     container.appendChild(div);
 }
-
 
 // reset button
 const resetBtn = document.querySelector('.reset');
