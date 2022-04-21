25ee99f77fb607a8bb9b63472ea92ac5ea1b7d3b
Shun <ter0123540@gmail.com>
2022-04-21 20:08:51 +0800

    use mouse to change color

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 const container = document.querySelector('.container');
 
 for (let i = 0; i < 256; i++) {
+    putDiv();
+}
+
+const  divs = document.querySelectorAll('div > div');
+divs.forEach(div => {
+    div.addEventListener('mouseover', (e) => {
+        e.target.style.backgroundColor = '#fa0';
+    })
+})
+
+function putDiv() {
     const div = document.createElement('div');
     container.appendChild(div);
 }
-console.log(container.length);
+
+
+// reset button
+const resetBtn = document.querySelector('.reset');
+resetBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    divs.forEach(div => {
+        div.style.backgroundColor = '';
+    })
+})
