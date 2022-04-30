6b4de2cfbee46a6f605f035df7902023f5678e68
Shun <ter0123540@gmail.com>
2022-04-30 11:04:59 +0800

    change the function of random-color

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
 const allColor = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f',];
-let randomColor = [];
-
+let color = '#000';
+let customizedSize = 0;
 
 const container = document.querySelector('.container');
 const resetBtn = document.querySelector('.reset');
 const colorChoice = document.querySelector('.color');
 const eraser = document.querySelector('.fa-eraser');
 const random = document.querySelector('.fa-question');
-let color = '#000';
-let customizedSize = 0;
 
 setInitialCanvas();
 const  divs = document.querySelectorAll('div > div');
-printNormalDot();
+normalColorMode();
+
+
+// function for setting canvas
 
 function setInitialCanvas() {
     for (let i = 0; i < 32*32; i++) {
         putDiv();
     }
     container.classList.add('gridBoard');
+}
+
+function putDiv() {
+    const div = document.createElement('div');
+    container.appendChild(div);
 }
 
 function setCustomizedCanvas() {
@@ -29,27 +35,28 @@ function setCustomizedCanvas() {
     container.style.gridTemplateRows = `repeat(${customizedSize}, 1fr)`;
 }
 
-function printNormalDot() {
-    divs.forEach(div => {
-        div.addEventListener('mouseover', (e) => {
-            e.target.style.backgroundColor = color;
+
+// function for printing
+function printNormalColor(e) {
+    e.target.style.backgroundColor = color;
+}
+
+function normalColorMode() {
+    container.addEventListener('mousedown', () => {
+        divs.forEach(div => {
+            div.addEventListener('mouseover', printNormalColor);
+        })
+    })
+    
+    container.addEventListener('mouseup', () => {
+        divs.forEach(div => {
+            div.removeEventListener('mouseover', printNormalColor);
         })
     })
 }
 
-function printRandomDot() {
-    divs.forEach(div => {
-        div.addEventListener('mouseover', (e) => {
-            changeColor();
-            e.target.style.backgroundColor = `#${randomColor.pop()}${randomColor.pop()}${randomColor.pop()}`;
-        })
-    })
-}
 
-function putDiv() {
-    const div = document.createElement('div');
-    container.appendChild(div);
-}
+// function for effect
 
 function reset() {
     divs.forEach(div => {
@@ -58,33 +65,34 @@ function reset() {
 }
 
 function changeColor() {
-    for(let i = 0; i < 3; i++) {
-        randomColor.push(allColor[Math.floor((Math.random() * allColor.length))])
-;    }
+    color = '#';
+    for(let i = 0; i < 6; i++) {
+        color += allColor[Math.floor((Math.random() * allColor.length))];
+    }
 }
 
-//  buttons
+// button manipulate
 
 colorChoice.addEventListener('input', () => {
     color = colorChoice.value;
-    printNormalDot();
+    normalColorMode();
 })
 
 colorChoice.addEventListener('click', () => {
     color = colorChoice.value;
-    printNormalDot();
+    normalColorMode();
+})
+
+resetBtn.addEventListener('click', (e) => {
+    reset();
 })
 
 eraser.addEventListener('click', () => {
     color = '#fff';
-    printNormalDot();
+    normalColorMode();
 })
 
 random.addEventListener('click', () => {
-    printRandomDot();
-})
-
-resetBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    reset();
+    changeColor();
+    normalColorMode();
 })
